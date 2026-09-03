bf3c90c55741effa7eb32a39c33865897b2ed6dc
saliboy22 <saleemoyarebu22@gmail.com>
2026-09-03 20:08:04 +0100

    Delete test_translator.py

--- test_translator.py
+++
@@ -1,9 +0,0 @@
-from ai_translator import AITranslator
-
-translator = AITranslator()
-
-text = "This medication may cause gastrointestinal bleeding and should be administered according to the prescribed dosage."
-
-result = translator.translate(text)
-
-print(result)
